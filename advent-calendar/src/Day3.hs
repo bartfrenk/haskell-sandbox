bd6abfbee7f02b4b34169c2283effa2a69fac860
Bart Frenk <bart.frenk@gmail.com>
2017-12-05 23:56:01 +0100

    Add solution to Day 3 of advent calendar

--- advent-calendar/src/Day3.hs
+++ advent-calendar/src/Day3.hs
@@ -1,10 +1,37 @@
 module Day3 where
 
-isOddSquare :: Integer -> Bool
-isOddSquare = undefined
+import Math.NumberTheory.Powers.Squares
 
-coordinates :: Integer -> (Int, Int)
+exactOddSquareRoot :: Integer -> Maybe Integer
+exactOddSquareRoot n = case exactSquareRoot n of
+  Just k -> if k `mod` 2 == 1 then Just k else Nothing
+  Nothing -> Nothing
+
+coordinates :: Integer -> (Integer, Integer)
 coordinates n
   | n == 1 = (0, 0)
-  | isOddSquare n = (0, 0)
-  | otherwise = (1, 1)
+  | otherwise = case exactOddSquareRoot n of
+      Just k ->
+        let c = (k - 1) `div` 2
+        in (c, -c)
+      Nothing ->
+        let (x, y) = coordinates (n - 1)
+        in descend (x, y)
+
+descend :: (Integer, Integer) -> (Integer, Integer)
+descend (x, y)
+  | x == y && x > 0 = (x - 1, y)
+  | x == y && x < 0 = (x + 1, y)
+  | x == -y && x > 0 = (x + 1, y)
+  | x == -y && x < 0 = (x, y - 1)
+  | abs x > abs y && x > 0 = (x, y + 1)
+  | abs x > abs y && x < 0 = (x, y - 1)
+  | abs x < abs y && y > 0 = (x - 1, y)
+  | abs x < abs y && y < 0 = (x + 1, y)
+  | otherwise = error "cannot happen"
+
+distance :: Integer -> Integer
+distance n = abs x + abs y
+  where (x, y) = coordinates n
+
+
